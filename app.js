6d3cdab073d609f131bbde7fbef04f184f755eb2
ebsPr <saidabalsells@hotmail.com>
2015-06-29 21:53:47 -0300

    screen question and answer with a question

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
 var express = require('express');
+var config = require('./config.js');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var quiz = require('./controllers/quiz_controllers.js');
 
 var app = express();
+// módulo de condiguración
+config(app);
+// módulo quiz
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.get('/quizes/question',quiz.question);
+app.get('/quizes/answer',quiz.answer);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/images/favicon.ico'));
-app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ controllers/quiz_controllers.js
@@ -0,0 +1,25 @@
+var quiz = (function(){
+
+    var question = function(req,res){
+        res.render('quizes', { quiz: 'Capital de Italia' });
+    };
+
+    var answer = function(req,res){
+        console.log(req.query.answer);
+        var result = "Respuesta incorrecta";
+        if (req.query.answer.trim().toUpperCase() == "ROMA"){
+            result = "Respuesta correcta";
+        }
+        res.render('answer', { result: result });
+    }
+    
+    return {
+    
+        question:question,
+        answer:answer
+    }
+   
+    
+})();
+
+module.exports = quiz;
