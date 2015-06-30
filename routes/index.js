32ff500cace09632c59cce3377f4617378ed766f
ebsPr <saidabalsells@hotmail.com>
2015-06-30 17:39:59 -0300

    added author screen 1739

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/author', function(req, res) {
+  res.render('author');
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Bienvenido a Quiz EBS' });
